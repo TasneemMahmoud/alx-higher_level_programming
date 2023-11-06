76389156778ac390a605dfae42f3a8e204ff969d
TasneemMahmoud <me@example.com>
2023-11-05 17:27:18 -0800

    Data structurers tasks

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_string.replace('C', '')
-    my_string.replace('c', '')
-    return my_string
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
