2fa8101e81c74725fdbf59665debd9adb0dce66c
TasneemMahmoud <me@example.com>
2023-11-20 16:59:41 -0800

    exceptions

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    """
+    a function that prints an integer.
+    Args:
+    value: can be any type (integer, string, etc.)
+    Returns:
+    True if value has been correctly printed (it means the value is an integer)
+    Otherwise, returns False and prints in stderr the error
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
